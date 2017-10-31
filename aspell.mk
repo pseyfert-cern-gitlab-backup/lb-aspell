f8f8281e73af212701e6ca8549dab81d4926f634
Paul Seyfert <pseyfert@cern.ch>
2017-10-31 11:42:42 +0100

    tweak gitlab artifact configuration

--- aspell.mk
+++ aspell.mk
@@ -13,3 +13,19 @@ batch-spellcheck: $(CHECKFILES:%.tex=%.batch-spellcheck)
 # if pdflatex fails, the output pdf file does not get deleted (as opposed to gcc)
 # force target deletion with:
 .DELETE_ON_ERROR:
+
+# lb-aspell, my addition of aspell to the LHCb document template
+# Copyright (C) 2017  Paul Seyfert <pseyfert@cern.ch>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- spellcheck.sh
+++ spellcheck.sh
@@ -8,3 +8,19 @@ else
   cat $1 | aspell list -t -d en_GB -p ./.aspell.en.pws | tee -a spelling-mistakes.txt
   exit 1
 fi
+
+# lb-aspell, my addition of aspell to the LHCb document template
+# Copyright (C) 2017  Paul Seyfert <pseyfert@cern.ch>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
